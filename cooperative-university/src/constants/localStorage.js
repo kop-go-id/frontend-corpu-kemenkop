d40f61aa8433d7c0169a15586c24442fc2a20c7a
Rama Diaz <ramadiaz221@gmail.com>
2025-10-28 13:22:50 +0700

    chore: remove unused files and configurations, including .gitignore, authentication documentation, and project setup files

--- cooperative-university/src/constants/localStorage.js
+++
@@ -1,6 +0,0 @@
-// LocalStorage key constants
-export const AUTH_TOKEN = 'AUTH_TOKEN';
-export const USER_DATA = 'USER_DATA';
-export const REFRESH_TOKEN = 'REFRESH_TOKEN';
-export const THEME_PREFERENCE = 'THEME_PREFERENCE';
-export const LANGUAGE_PREFERENCE = 'LANGUAGE_PREFERENCE';
